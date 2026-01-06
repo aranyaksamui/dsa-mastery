c103e0e5ae942a7f90163db868047a7321aa84d4
Aranyak Samui <aranyaksamuiarya@gmail.com>
2026-01-06 22:47:47 +0530

    Initial commit

---
+++ cpp/template.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+
+// This function speeds up input/output operations
+void fast_io()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+}
+
+// Solution Class (Like LeetCode)
+class Solution
+{
+public:
+    void solve()
+    {
+        // Your code will go here
+        std::cout << "System ready." << std::endl;
+    }
+};
+
+int main()
+{
+    fast_io();
+    Solution sol;
+    sol.solve();
+    return 0;
+}
