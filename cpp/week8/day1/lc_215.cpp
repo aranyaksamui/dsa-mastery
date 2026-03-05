6a17e78648a3affa63b1d49313d97a3709f5fb0b
Aranyak Samui <aranyaksamuiarya@gmail.com>
2026-03-06 00:46:28 +0530

    Complete day 2 of week 8. Solved LeetCode 973 and 1046.

--- cpp/week8/day1/lc_215.cpp
+++ cpp/week8/day1/lc_215.cpp
@@ -14,17 +14,6 @@
 
 using namespace std;
 
-// Definition for a binary tree node.
-struct TreeNode 
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 // This function speeds up input/output operations
 void fast_io()
 {
